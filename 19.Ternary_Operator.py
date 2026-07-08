15a9dea69bd6d22c659bad476c8ec53c7b7158ee
Vishwadev Gupta <75661083+vishwadevgupta@users.noreply.github.com>
2026-07-08 18:46:24 +0530

    commits

---
+++ 19.Ternary_Operator.py
@@ -0,0 +1,8 @@
+#TERNARY OPERATOR
+
+is_raining = True
+
+is_wet = "Yes" if is_raining else "No" # Here it will check if is_raining is True or False,
+                                       # if True it will assign "Yes" to is_wet, otherwise it will assign "No"
+
+print(is_wet)
