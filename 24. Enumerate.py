b5a3c017908d97e02dde7ef3e3d7e3dd40b2c1a3
vishwadevgupta <75661083+vishwadevgupta@users.noreply.github.com>
2026-07-13 05:52:47 +0000

    style: auto-reformat all files with issues

--- 24. Enumerate.py
+++ 24. Enumerate.py
@@ -1,4 +1,6 @@
-#ENUMERATE
+# ENUMERATE
 
-for index, value in enumerate(['apple', 'banana', 'cherry']): # here we are using the enumerate function to get both the index and the value of each item in the list
+for index, value in enumerate(
+    ["apple", "banana", "cherry"]
+):  # here we are using the enumerate function to get both the index and the value of each item in the list
     print(index, value)
--- 25.Exercise( finding Duplicates ).py
+++ 25.Exercise( finding Duplicates ).py
@@ -1,35 +1,35 @@
 # FIND DUPLICATES IN LIST
 # 1. Solution (without using anything just loops and ifs)
 
-NList = ['a','c','b','a']
+NList = ["a", "c", "b", "a"]
 DList = []
 i = 0
-j = 0 
-counter =0
+j = 0
+counter = 0
 for items in NList:
-    i=0 
+    i = 0
     for items2 in NList:
         if i != j:
             if items2 == items:
                 if items2 not in DList:
                     DList.append(items2)
         i += 1
-    j+= 1
-print (DList)
+    j += 1
+print(DList)
 
-#2 Using Enumerates
+# 2 Using Enumerates
 
-NList = ['a','c','b','a','e','f','j','c','k','f']
+NList = ["a", "c", "b", "a", "e", "f", "j", "c", "k", "f"]
 DList = []
-for i,items in enumerate(NList):
-    for j,items2 in enumerate(NList):
+for i, items in enumerate(NList):
+    for j, items2 in enumerate(NList):
         if i != j and items2 == items and items2 not in DList:
             DList.append(items2)
 print(DList)
 
-#3 using count function;
+# 3 using count function;
 
-NList = ['a','c','b','a','e','f','j','c','k','f']
+NList = ["a", "c", "b", "a", "e", "f", "j", "c", "k", "f"]
 DList = []
 
 for items in NList:
@@ -37,6 +37,3 @@ for items in NList:
         DList.append(items)
 
 print(DList)
-
-            
-        
