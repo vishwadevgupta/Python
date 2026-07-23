8b2e012905850d2eeeb631ddabb19794ad3abc1f
vishwadevgupta <75661083+vishwadevgupta@users.noreply.github.com>
2026-07-23 05:00:29 +0000

    style: auto-reformat all files with issues

--- 28,Exercise(Highest_even_function).py
+++ 28,Exercise(Highest_even_function).py
@@ -1,24 +1,27 @@
-#HIGHEST EVEN FUCNTION
+# HIGHEST EVEN FUCNTION
 
 
 def high_even(li):
     high = 0
     for num in li:
-        if num % 2 == 0 and num > high :
-            high = num 
+        if num % 2 == 0 and num > high:
+            high = num
     return high
 
-print(high_even([1,2,4,3,8,6]))
-    
 
-# if we have negative numbers in the list. 
+print(high_even([1, 2, 4, 3, 8, 6]))
+
+
+# if we have negative numbers in the list.
+
 
 def high_even(li):
     high = None
     for num in li:
-        if num % 2 == 0 :
-            if high is None or num > high :
-                high = num 
+        if num % 2 == 0:
+            if high is None or num > high:
+                high = num
     return high
 
-print(high_even([1,2,4,3,8,6]))
+
+print(high_even([1, 2, 4, 3, 8, 6]))
